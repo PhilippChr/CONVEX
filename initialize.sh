4a56df78e7eec90fefa1e1e81d355f588a7009d9
Philipp Christmann <philipp-christmann@web.de>
2019-10-10 23:05:38 +0200

    minor fix bash-script

--- initialize.sh
+++ initialize.sh
@@ -5,4 +5,4 @@ wget http://gaia.infor.uva.es/hdt/wikidata/wikidata2018_09_11.hdt.gz
 unzip wikidata2018_09_11.hdt.gz
 mv wikidata2018_09_11.hdt data
 wget http://qa.mpi-inf.mpg.de/convex/ConvQuestions_test.zip
-unzip ConvQuestions_test.zip data/
+unzip ConvQuestions_test.zip -d data/
